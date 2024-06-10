d233311ea30868d790bd8664c105bc530797caa4
Moomink <develop@6mink.dev>
2024-06-11 01:48:37 +0900

    mod: plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -63,6 +63,36 @@ require('pckr').add {
 
 
     -- Utility
+
+    {
+        "folke/noice.nvim",
+        requires =  {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        },
+        config = function()
+            local noice = require("noice")
+            noice.setup({
+                lsp = {
+                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                    },
+                },
+            -- you can enable a preset for easier configuration
+                presets = {
+                    bottom_search = true, -- use a classic bottom cmdline for search
+                    command_palette = true, -- position the cmdline and popupmenu together
+                    long_message_to_split = true, -- long messages will be sent to a split
+                    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                    lsp_doc_border = false, -- add a border to hover docs and signature help
+                },
+            })
+    	    end
+    },
+
     --- mini.nvim
     {
         'echasnovski/mini.map',
@@ -176,17 +206,15 @@ require('pckr').add {
 
 
     -- Language Server Protocol
-    {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
-        'lukas-reineke/lsp-format.nvim',
-        'hrsh7th/nvim-cmp',
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-cmdline',
-        'stevearc/conform.nvim',
-    },
-
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    'lukas-reineke/lsp-format.nvim',
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/vim-vsnip',
+    'hrsh7th/cmp-cmdline',
+    'stevearc/conform.nvim',
 }
